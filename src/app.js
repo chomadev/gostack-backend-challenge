6301af2413a2772b64b72cf0741df64d1a1ece89
Marcos Choma <marcos.choma@gmail.com>
2020-08-19 00:03:39 -0300

    Implementing API for repositories, with HTTP methods GET, POST, PUT and DELETE, with additional POST method for likes in repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,61 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next) {
+  const { id } = request.params;
+  if (!id)
+    return response.sendStatus(400);
+
+  const index = getIndexFromId(id);
+  if (index < 0) {
+    return response.sendStatus(400);
+  }
+
+  next();
+}
+
+function getIndexFromId(id) {
+  return repositories.findIndex(repository => repository.id === id);
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = getIndexFromId(id);
+  const repository = repositories[index];
+
+  if (title) repository.title = title;
+  if (url) repository.url = url;
+  if (techs) repository.techs = techs;
+
+  repositories[index] = repository;
+
+  return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateId, (request, response) => {
+  const { id } = request.params;
+  repositories.splice(getIndexFromId(id), 1);
+  return response.sendStatus(204);
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateId, (request, response) => {
+  const { id } = request.params;
+  const index = getIndexFromId(id);
+  repositories[index].likes++;
+  return response.status(200).json(repositories[index]);
 });
 
 module.exports = app;
